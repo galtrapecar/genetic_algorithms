49d1364b9f131464cff07ca0b945b665d0cf56e8
Gal Trapečar <trapecar.gal@gmail.com>
2023-05-13 15:39:02 +0200

    js genetic algorithm

---
+++ src/components/Population/Population.tsx
@@ -0,0 +1,51 @@
+import React, { useEffect, useRef, useState } from "react";
+import { fitness, init, new_population } from "../../genetic/genetic";
+import './Population.css'
+import { IGenotype } from "../../genetic/helpers";
+
+export function Population() {
+    const [phenotypes, setPhenotypes] = useState(init())
+    const firedRef = useRef(false);
+
+    useEffect(() => {
+        if (firedRef.current) return
+        firedRef.current = true
+
+        // window.addEventListener('click', evolve)
+        evolve()
+    }, [])
+
+    async function evolve() {
+
+        let genotypes = [...phenotypes]
+
+        let i = 0
+        while (i < 10000) {
+
+            await new Promise(r => setTimeout(r, 20));
+    
+            let population = [...new_population(genotypes)]
+            setPhenotypes(population)
+
+            if (population.length == 16) break
+            
+            genotypes = population
+
+            i++
+        }
+    }
+
+    return (
+        <div className="Population">
+            <div className="inner">
+                {phenotypes.map(X => {
+                    return (
+                        <div className="bar" style={{backgroundColor: X.color, height: X.height}}>
+                            {X.fitness}    
+                        </div>
+                    )
+                })}
+            </div>
+        </div>
+    )
+}
